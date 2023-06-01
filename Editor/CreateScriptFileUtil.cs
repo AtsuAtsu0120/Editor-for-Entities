1d745c1ddfcf29930b1a7951fa25d34ead97e29c
AtsuAtsu <58623243+AtsuAtsu0120@users.noreply.github.com>
2023-06-01 21:39:14 +0900

    [Fix]Cant create script

--- Editor/CreateScriptFileUtil.cs
+++ Editor/CreateScriptFileUtil.cs
@@ -3,7 +3,7 @@ using UnityEditor;
 
 public static class CreateScriptFileUtil
 {
-    private const string PLUGIN_PATH = "Assets/Plugins/EditorForECS/";
+    private const string PLUGIN_PATH = "Packages/EditorForECS/";
     public static void CreateScriptFile(string fileName, string newFileName)
     {
         ProjectWindowUtil.CreateScriptAssetFromTemplateFile(System.IO.Path.Combine(PLUGIN_PATH, $"Templates/{fileName}"), newFileName);
