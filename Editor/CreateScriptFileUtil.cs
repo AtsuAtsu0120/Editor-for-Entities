6a0ba600f0c37eadb7195f75978aa2695e8d1984
AtsuAtsu <58623243+AtsuAtsu0120@users.noreply.github.com>
2023-06-01 21:14:07 +0900

    first commit

---
+++ Editor/CreateScriptFileUtil.cs
@@ -0,0 +1,11 @@
+using Unity.VisualScripting;
+using UnityEditor;
+
+public static class CreateScriptFileUtil
+{
+    private const string PLUGIN_PATH = "Assets/Plugins/EditorForECS/";
+    public static void CreateScriptFile(string fileName, string newFileName)
+    {
+        ProjectWindowUtil.CreateScriptAssetFromTemplateFile(System.IO.Path.Combine(PLUGIN_PATH, $"Templates/{fileName}"), newFileName);
+    }
+}
---
+++ Editor/EntitiesEditorWindow.cs
@@ -0,0 +1,36 @@
+using UnityEditor;
+
+public class EntitiesEditorWindow : EditorWindow
+{
+    private const string MENUROOT = "Assets/Create/Editor for Entites/";
+    [MenuItem(MENUROOT + "Component/UnmanagedComponent", priority = 10)]
+    private static void CreateComponentDataCS()
+    {
+        CreateScriptFileUtil.CreateScriptFile("IComponent.txt", "ComponentData.cs");
+    }
+    [MenuItem(MENUROOT + "Component/ManagedComponent", priority = 11)]
+    private static void CreateManagedComponentDataCS()
+    {
+        CreateScriptFileUtil.CreateScriptFile("ManagedIComponent.txt", "ManagedComponentData.cs");
+    }
+    [MenuItem(MENUROOT + "Component/IBufferElementData", priority = 12)]
+    private static void CreateBufferElementCS()
+    {
+        CreateScriptFileUtil.CreateScriptFile("IBuffer.txt", "BufferElement.cs");
+    }
+    [MenuItem(MENUROOT + "System/ISystem", priority = 13)]
+    private static void CreateSystemCS()
+    {
+        CreateScriptFileUtil.CreateScriptFile("ISystem.txt", "System.cs");
+    }
+    [MenuItem(MENUROOT + "System/SystemBase", priority = 14)]
+    private static void CreateSystemBaseCS()
+    {
+        CreateScriptFileUtil.CreateScriptFile("SystemBase.txt", "SystemBase.cs");
+    }
+    [MenuItem(MENUROOT + "Authoring", priority = 15)]
+    private static void CreateAuthoringCS()
+    {
+        CreateScriptFileUtil.CreateScriptFile("Baker.txt", "Authoring.cs");
+    }
+}
